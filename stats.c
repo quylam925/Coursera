1a5738844100cb718fa46579a0a09faabec6a4b1
Quy Lam Hoang <quylamhust@gmail.com>
2017-09-06 03:00:16 -0600

    Add helloworld for testing

--- stats.c
+++ stats.c
@@ -35,6 +35,7 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+  printf("Hello world\n");
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
   print_array(test,SIZE);
